e4c06e745849b4c027a7af55906bdafaed11f2b8
Graham Arnett <GrahamKentArnett@outlook.com>
2024-11-06 14:27:59 -0500

    init commit

---
+++ Even Number Checker App/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Even_Number_Checker_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the Even number checker app");
+
+            String choice;
+            int entry;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please enter an integer to check: ");
+                entry = int.Parse(Console.ReadLine());
+              
+
+                if (entry % 2 == 0)
+                {
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine("This is even.");
+                }
+                else 
+                {
+                    Console.WriteLine("This is odd.");
+                }
+
+            Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            }while(choice == "y");
+        }
+    }
+}
